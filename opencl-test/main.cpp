a96a6b8068319fee79446a2b3f9f71372f41fbb5
Markus Mayer <awesome@everybag.de>
2016-09-12 20:43:35 +0200

    added basic context handling

--- opencl-test/main.cpp
+++ opencl-test/main.cpp
@@ -12,19 +12,45 @@ void main()
     clGetPlatformIDs(0, nullptr, &platformIdCount);
 
     // get all platforms
-    std::vector<cl_platform_id> platformIds(platformIdCount);
+    vector<cl_platform_id> platformIds(platformIdCount);
     clGetPlatformIDs(platformIdCount, platformIds.data(), nullptr);
 
-    for (const auto id : platformIds)
+    for (const auto platformId : platformIds)
     {
         size_t length;
-        clGetPlatformInfo(id, CL_PLATFORM_NAME, 0, nullptr, &length);
 
-        auto* value = new char[length];
-        clGetPlatformInfo(id, CL_PLATFORM_NAME, length, value, nullptr);
+        // get the platforn name
+        clGetPlatformInfo(platformId, CL_PLATFORM_NAME, 0, nullptr, &length);
+        auto* platformName = new char[length];
+        clGetPlatformInfo(platformId, CL_PLATFORM_NAME, length, platformName, nullptr);
 
-        cout << "Platform " << id << ": " << value << endl;
-        delete[] value;
+        // get the platform version
+        clGetPlatformInfo(platformId, CL_PLATFORM_NAME, 0, nullptr, &length);
+        auto* platformVersion = new char[length];
+        clGetPlatformInfo(platformId, CL_PLATFORM_VERSION, length, platformVersion, nullptr);
+
+        cout << platformName << " (" << platformVersion << ")" <<  endl;
+        delete[] platformName;
+        delete[] platformVersion;
+
+        // get the number of defines on the first platform
+        cl_uint deviceIdCount = 0;
+        clGetDeviceIDs(platformId, CL_DEVICE_TYPE_ALL, 0, nullptr, &deviceIdCount);
+
+        // get the devices on the first platform
+        vector<cl_device_id> deviceIds(deviceIdCount);
+        clGetDeviceIDs(platformId, CL_DEVICE_TYPE_ALL, deviceIdCount, deviceIds.data(), nullptr);
+
+        for (const auto deviceId : deviceIds)
+        {
+            // get the device name
+            clGetDeviceInfo(deviceId, CL_DEVICE_NAME, 0, nullptr, &length);
+            auto* deviceName = new char[length];
+            clGetDeviceInfo(deviceId, CL_DEVICE_NAME, length, deviceName, nullptr);
+
+            cout << "  - " << deviceName << endl;
+            delete[] deviceName;
+        }
     }
 
     // get the number of defines on the first platform
@@ -32,6 +58,24 @@ void main()
     clGetDeviceIDs(platformIds[0], CL_DEVICE_TYPE_ALL, 0, nullptr, &deviceIdCount);
 
     // get the devices on the first platform
-    std::vector<cl_device_id> deviceIds(deviceIdCount);
+    vector<cl_device_id> deviceIds(deviceIdCount);
     clGetDeviceIDs(platformIds[0], CL_DEVICE_TYPE_ALL, deviceIdCount, deviceIds.data(), nullptr);
+
+    // get a context on the first platform
+    const cl_context_properties contextProperties[] =
+    {
+        CL_CONTEXT_PLATFORM,
+        reinterpret_cast<cl_context_properties> (platformIds[0]),
+        0, 0
+    };
+
+    cout << "Creating the context ..." << endl;
+    cl_int error;
+    auto context = clCreateContext(
+        contextProperties, deviceIdCount,
+        deviceIds.data(), nullptr,
+        nullptr, &error);
+
+    cout << "Releasing the context ..." << endl;
+    clReleaseContext(context);
 }
